07362e0ffc9d22f4ff74cec7d88e3729b6195cd3
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-04-04 22:50:40 +0200

    comments

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ server.use(restify.bodyParser());
 server.use(restify.CORS({}));
 server.use(restify.queryParser());
 
+
+//CORS
 server.opts(/\/.*/, function (req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
@@ -17,17 +19,23 @@ server.opts(/\/.*/, function (req, res, next){
   res.send(200);
 });
 
+
+// serve public folder
 server.get(/^(?!\/book).*/, restify.serveStatic({
   directory: './public/',
   default: 'index.html'
 }));
 
+
+// API routes
 server.get('/books', bookStoreController.getAll);
 server.post('/book', bookStoreController.create);
 server.get('/book/:isbn', bookStoreController.getByISBN);
 server.put('/book/:isbn', bookStoreController.update);
 server.del('/book/:isbn', bookStoreController.delete);
 
+
+// start server
 server.listen(port, function() {
   console.log('Server running at %s', server.url);
 });
