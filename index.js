d6c9d9b07c32e6bc57107c13e01c0a5b92930b6d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-08-08 18:42:56 +0200

    Revert "this is no longer an ember-cli addon, we have to re-implement this for webpack"
    This reverts commit bb6673812b025a91f65719b8a568c52d0ad0e718.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// TODO: this is no longer an ember-cli addon, we have to re-implement this for webpack
+module.exports = {
+  
+  name: 'book-monkey2-api',
+  
+  serverMiddleware: function (config) {
+  
+    // in future we might want to integrate our middleware into existing express-server
+    // example: https://github.com/rwjblue/ember-cli-content-security-policy/blob/v0.3.0/index.js#L25    
+    var server = require('./server.js');
+  }
+};
