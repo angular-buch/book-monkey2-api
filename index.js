ea6964f05728220838bd6c3b648f08ff63bfa38a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-08-18 09:37:06 +0200

    angular-cli@webpack, switching from serverMiddleware to normal command. start the server with `ng monkey`

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
+'use strict';
+
+// `require.main.require` so that plugin even works when linked with `npm link`
+// see https://github.com/npm/npm/issues/5875 / http://stackoverflow.com/a/25800501
+//
+// hint:
+// this asumes a flat node_modules structure with ember-cli on top
+// and does not work in NPM 2 until ember-cli is directly in package.json
+var Command = require.main.require('ember-cli/lib/models/command');
+var Promise = require.main.require('ember-cli/lib/ext/promise');
+
+var cmd = Command.extend({
+
+  name: 'monkey',
+  description: 'Starts a webserver with the BookMonkey2 API',
+  works: 'everywhere',
+  //aliases: ['server', 's'],
+
+  run: function (commandOptions) {
+
+    // same as in serve-webpack.ts, it never resolves so that it runs endlessly
+    return new Promise((resolve, reject) => { 
+
+        // this can throw exceptions, eg. when port is blocked
+        var server = require('./server.js');
+    });
+  }
+});
+
+
 module.exports = {
-  
   name: 'book-monkey2-api',
-  
-  serverMiddleware: function (config) {
-  
-    // in future we might want to integrate our middleware into existing express-server
-    // example: https://github.com/rwjblue/ember-cli-content-security-policy/blob/v0.3.0/index.js#L25    
-    var server = require('./server.js');
+
+  includedCommands: function () {
+    return {
+      'monkey': cmd
+    };
   }
 };
