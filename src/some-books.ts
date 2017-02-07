6792aa1fd760ca39379a8b26cc3b37482fcd8c70
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-02-07 22:35:24 +0100

    redirects for both cover pictures of book v1 and book v2

--- src/some-books.ts
+++ src/some-books.ts
@@ -10,7 +10,7 @@ export class SomeBooks {
         new Date('2017-04-01T12:00:00.000Z'),
         'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen',
         5,
-        [new Thumbnail('https://angular-buch.com/angular-buch.jpg', 'Front Cover')],
+        [new Thumbnail('https://ng-buch.de/cover2.jpg', 'Front Cover')],
         'Mit diesem Buch arbeiten Sie sich durch Googles neuestes Framework und erstellen mit aktuellen Technologien moderne Webanwendungen. Lernen und vertiefen Sie die komponentenorientierte Entwicklung von Single-Page-Anwendungen. Neben den Grundlagen werden auch fortgeschrittene Konzepte von Angular am Beispiel erklärt.'),
       new Book(
         '3864901545',
@@ -19,7 +19,7 @@ export class SomeBooks {
         new Date('2014-05-01T00:00:00.000Z'),
         'Eine praktische Einführung',
         4,
-        [new Thumbnail('https://www.dpunkt.de/images/cover/detail/4489.jpg', 'Front Cover')],
+        [new Thumbnail('https://ng-buch.de/cover1.jpg', 'Front Cover')],
         'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts schrittweise an die Entwicklung mit dem clientseitigen Webframework AngularJS heran. Lernen Sie dabei Konzepte und Techniken kennen, mit denen Sie strukturierte, modularisierte und gut wartbare JavaScript-Webapplikationen erstellen können.'
       )
     ]
